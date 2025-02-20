ecbee01163533c8f5c373d91ea2ffae2bf9876ef
Dragos Trandafir <62999548+DragosTrandafir@users.noreply.github.com>
2025-02-20 15:35:22 +0200

    changed variable names to be clearer

--- WordLadder2/Node.cpp
+++ WordLadder2/Node.cpp
@@ -3,14 +3,14 @@
 
 map<string, set<string>> pattern_words;
 vector<string> random_words;
-ostream& operator<<(ostream& os2, const Graph<string>& g) {
-    for (const auto& node : g.adjacency_lists) {
-        os2 << node.first << "   second ";
+ostream& operator<<(ostream& outputStream, const Graph<string>& graph) {
+    for (const auto& node : graph.adjacency_lists) {
+        outputStream << node.first << "   second ";
         for (const auto& neighbor : node.second)
-            os2 << neighbor << " ";
-        os2 << std::endl;
+            outputStream << neighbor << " ";
+        outputStream << std::endl;
     }
-        return os2;
+        return outputStream;
 }
 
 void form_n_letters_map(int n, const string& path) {
@@ -28,19 +28,19 @@ void form_n_letters_map(int n, const string& path) {
             }
 }
 
-vector<string> find_transformation(const string& start, const string& end, const Graph<string>& g) {
+vector<string> find_transformation(const string& start, const string& end, const Graph<string>& graph) {
     map<string, string> parent;
-    queue<string> q;
+    queue<string> words_queue;
     set<string> visited;
     vector<string> path;
 
-    q.push(start);
+    words_queue.push(start);
     visited.insert(start);
     parent[start] = start;
 
-    while (!q.empty()) {
-        string current = q.front();
-        q.pop();
+    while (!words_queue.empty()) {
+        string current = words_queue.front();
+        words_queue.pop();
 
         if (current == end) {
             // Found the end word, print the path
@@ -57,9 +57,9 @@ vector<string> find_transformation(const string& start, const string& end, const
         }
 
         // Traverse neighbors of current node
-        for (const auto& neighbor : g.adjacency_lists.at(current)) {
+        for (const auto& neighbor : graph.adjacency_lists.at(current)) {
             if (visited.find(neighbor) == visited.end()) {
-                q.push(neighbor);
+                words_queue.push(neighbor);
                 visited.insert(neighbor);
                 parent[neighbor] = current; // Set the parent of neighbor to current node
             }
